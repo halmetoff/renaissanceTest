35070398b14578c58bbfdb162ff5aea9af8603bb
Vladislav Halmetov <halmetoff@gmail.com>
2021-03-26 09:18:44 +0300

    Произведено форматирование.

--- src/test/java/ru/rencredit/page/MainPage.java
+++ src/test/java/ru/rencredit/page/MainPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 public class MainPage {
 
     public WebDriver driver;
+
     public MainPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
